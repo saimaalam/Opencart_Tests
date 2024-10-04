d2f5e3e2f6b2d2b98dcc48b1ee17e987bbf3b54c
Saima <saimaalam.dsi@gmail.com>
2024-10-04 14:41:58 +0600

    init

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,6 +1,7 @@
 package login;
 
 import base.BaseTests;
+import io.qameta.allure.*;
 import opencart_pages.LoginPage;
 import opencart_pages.MyAccoutPage;
 import org.testng.Assert;
@@ -10,13 +11,26 @@ import static org.testng.Assert.assertEquals;
 
 public class LoginTests extends BaseTests {
     @Test(dataProvider = "LoginData",dataProviderClass =LoginTestDataGenerator.class)
+    @Description("Test for user login functionality")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("User Login")
     public void TC_4_Verify_that_user_can_login_with_valid_credentials(String email, String password){
+        Allure.step("Step 1: Open login page",() -> {
+            LoginPage loginPage= homePage.clickLogin();
+        });
+        Allure.step("Step 2: Enter email: " + email,()->{});
+        Allure.step("Step 3: Enter password");
+        Allure.step("Step 4: Click on login button");
         LoginTestDataGenerator loginData= new LoginTestDataGenerator();
         LoginPage loginPage= homePage.clickLogin();
-        loginPage.setEmailField(email);
-        loginPage.setPasswordField(password);
+        loginPage.setEmailField("OzzieUpton@yahoo.com");
+        loginPage.setPasswordField("123456");
         MyAccoutPage myAccoutPage= loginPage.clickLoginButton();
         Assert.assertEquals(myAccoutPage.getPageHeader(),"My Account","Header didn't match!!!");
+    }
+    @Test
+    public void TC_5_Verify_that_user_can_log_out(){
+
     }
 
 }
--- src/test/java/registration/RegistrationTests.java
+++ src/test/java/registration/RegistrationTests.java
@@ -1,6 +1,8 @@
 package registration;
 
 import base.BaseTests;
+import io.qameta.allure.Allure;
+import io.qameta.allure.*;
 import opencart_pages.AccountSuccessPage;
 import opencart_pages.RegisterAccountPage;
 import org.testng.annotations.*;
@@ -9,7 +11,18 @@ import utils.RegistrationTestDataGenerator;
 
 public class RegistrationTests extends BaseTests {
 @Test(dataProvider = "registrationData", dataProviderClass = RegistrationTestDataGenerator.class)
+@Description("Test for user registration functionality")
+@Severity(SeverityLevel.BLOCKER)
+@Feature("User Registration")
     public void TC_1_Verify_that_a_new_user_can_register(String firstName, String lastName, String email,String password){
+
+    Allure.step("Step 1: Open registration page");
+    Allure.step("Step 2: Enter first name: " + firstName);
+    Allure.step("Step 3: Enter last name: " + lastName);
+    Allure.step("Step 4: Enter email: " + email);
+    Allure.step("Step 5: Enter password");
+    Allure.step("Step 6: Submit the registration form");
+
     RegisterAccountPage registerAccountPage= homePage.clickRegister();
     registerAccountPage.waitForHeader();
     registerAccountPage.setFirstName(firstName);
